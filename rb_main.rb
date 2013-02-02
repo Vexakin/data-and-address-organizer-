c83ec52c0497a45644fb2a85ff1cbaed54115183
Adam Barber <adam@adambarber.tv>
2013-02-01 18:35:36 -0800

    Refactored script

--- rb_main.rb
+++ rb_main.rb
@@ -1,37 +1,64 @@
 #!/usr/bin/env ruby
-path = Dir["Contracts/*.txt"]
-filename ="Millage.txt"
-total = Dir["Contracts"].length
-i =0
 
+# Determine where to look for contracts
+path = Dir["Contracts/*.txt"]
+
+# Set file name of final output
+filename = "#{Time.now.strftime('%Y-%m-%d')}-Mileage.txt"
+
+# If the final output file already exists, then don't create it again
 if(File.exist?(filename) == false)
     File.new(filename, 'a')
-    puts "File Created"
-    else
-    
 end
 
+# Loop through the folder once for each file
+path.each do |file|
 
+  # Loop through each line in a given file
+  File.readlines(file).each do |line|
 
-while i <= total do
-        path.each do |file|
-            puts file
-            f = File.open(file, 'r')
-            a = f.readlines
-            adrs = a[11].strip
-            date = a[9].strip
-            date = date.split(' ').reject{|n|n== "Party"}.join(' ') 
-            print adrs
-            print date
-            File.open(filename, "a"){ |file| file.print date} 
-            File.open(filename, "a"){ |file| file.print "         "} 
-            File.open(filename, "a"){ |file| file.puts adrs} 
-             File.open(filename, "a"){ |file| file.puts "Total Distance:"} 
-            File.open(filename, "a"){ |file| file.puts ""} 
-            i = i+1
+    # if a given line contains "Party Date"
+    if line[/Party Date/]
+      # Once we've found a line, split the line into a 2 part array
+      # after ": " and grab everything in the array at index 1
+      @party_date = line.split(": ")[1]
     end
-end
 
-    
-    
-    
+    # if a given line contains "Client"
+    if line[/Client/]
+      # Once we've found a line, split the line into a 2 part array
+      # after ": " and grab everything in the array at index 1
+      @client = line.split(": ")[1]
+    end
+
+    # if a given line contains "Entertainment Time"
+    if line[/Entertainment Time/]
+      # Once we've found a line, split the line into a 2 part array
+      # after ": " and grab everything in the array at index 1
+      @entertainment_time = line.split(": ")[1]
+    end
+
+    # if a given line contains "Location"
+    if line[/Location/]
+      # Once we've found a line, split the line into a 2 part array
+      # after ": " and grab everything in the array at index 1
+      @location = line.split(": ")[1]
+    end
+
+  # if a given line contains "Contact Phone"
+    if line[/Contact Phone/]
+      # Once we've found a line, split the line into a 2 part array
+      # after ": " and grab everything in the array at index 1
+      @contact_phone = line.split(": ")[1]
+    end
+  end
+
+  # Combine the instance variables from the file into a single string
+  party_info = "#{@client} | #{@party_date} | #{@entertainment_time} | #{@location} | #{@contact_phone}"
+
+  # Remove any line breaks that happen to show up and shouldn't be there
+  party_info.gsub!(/(\S)[^\S\n]*\n[^\S\n]*(\S)/, '\1 \2')
+
+  # Write cleaned string to the final output file and add a single line break afterwards
+  File.open(filename, "a"){ |results_file| results_file.puts "#{party_info}\n"}
+end
